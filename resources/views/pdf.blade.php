34340f49f1dafc937671c4f6a55460ac90cde93c
miaroandy <100229471+miaroandy@users.noreply.github.com>
2023-07-20 08:26:38 +0300

    Update pdf.blade.php

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,15 +1,78 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Ma Facture</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f8f8;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+            color: #333;
+            text-transform: uppercase;
+        }
+
+        h4 {
+            margin-bottom: 10px;
+            color: #777;
+        }
+
+        p {
+            margin-bottom: 20px;
+            color: #555;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 10px;
+            border: 1px solid #ccc;
+            text-align: center;
+        }
+
+        th {
+            background-color: #f2f2f2;
+            color: #333;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        tr:last-child {
+            font-weight: bold;
+        }
+
+        .total-row {
+            background-color: #eee;
+        }
+
+        .total-label {
+            font-weight: bold;
+            text-align: right;
+        }
+
+        .total-amount {
+            font-weight: bold;
+            text-align: center;
+            background-color: yellow;
+        }
+    </style>
 </head>
 <body>
     <h1>Facture</h1>
-    <h4> Mr/Mme {{$facture->nom}} </h4>
-    <p>du {{$facture->date_facture}}</p>
+    <h4>Mr/Mme {{$facture->nom}}</h4>
+    <p>du {{ date('d F Y', strtotime($facture->date_facture)) }}</p>
 
-    <table border="1">
+    <table>
         <tr>
             <th>Acte</th>
             <th>Nombre</th>
@@ -28,18 +91,12 @@
             <td>{{$df->nombre}}</td>
             <td>{{$df->montant}}</td>
             <td>{{$df->montant_total}}</td>
-
         </tr>
         @endforeach
-        <tr>
-            <td>TOTAL</td>
-            <td></td>
-            <td></td>
-            <td>{{$t}}</td>
-
+        <tr class="total-row">
+            <td colspan="3" class="total-label">TOTAL</td>
+            <td class="total-amount">{{$t}}</td>
         </tr>
-
     </table>
-
-
 </body>
+</html>
